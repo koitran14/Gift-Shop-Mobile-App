082e83b38c1e3bdfbabe32f3c7926552107b43c3
ChaoZiK <tranthephong101@gmail.com>
2024-03-12 14:11:54 +0700

    Tuto 5 + 6

--- src/screens/index.js
+++ src/screens/index.js
@@ -3,5 +3,7 @@ import WelcomeScreen from "./WelcomeScreen";
 import SigninScreen from "./SigninScreen";
 import SignupScreen from "./SignupScreen";
 import ForgotPasswordScreen from "./ForgotPasswordScreen";
+import RegisterPhoneScreen from "./RegisterPhoneScreen";
+import VerificationScreen from "./VerificationScreen";
 
-export { SplashScreen, WelcomeScreen,SigninScreen,SignupScreen,ForgotPasswordScreen}
+export { SplashScreen, WelcomeScreen,SigninScreen,SignupScreen,ForgotPasswordScreen, RegisterPhoneScreen,VerificationScreen,};
